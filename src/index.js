addf80c1a0d9f458510cd91590ecb74bb78a83f6
sofiagakalo1 <108238510+sofiagakalo1@users.noreply.github.com>
2022-12-06 16:10:53 +0100

    imports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
